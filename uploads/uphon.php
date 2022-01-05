f331e7f6554c70756b7fe8a52d23b4e2d785638a
quyenkaneki <95172923+quyenkaneki@users.noreply.github.com>
2022-01-05 16:53:11 +0700

    Update uphon.php

--- uploads/uphon.php
+++ uploads/uphon.php
@@ -21,7 +21,7 @@ echo "UP ảnh gif hôn nhau\n";
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     //Bước 1: Tạo thư mục lưu file
     $error = array();
-    $target_dir = "https://apiquyenkaneki.tk/uploads/hôn/";
+    $target_dir = "hôn/";
     $target_file = $target_dir . basename($_FILES['fileUpload']['name']);
     // Kiểm tra kiểu file hợp lệ
     $type_file = pathinfo($_FILES['fileUpload']['name'], PATHINFO_EXTENSION);
